6e5f6ca93f07df6ed8d7416e6cc59ead7d3b01b9
bramwell simiyu <simiyub.engineer@gmail.com>
2020-06-12 09:27:25 +0100

    added a simple form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <h4>Just demos</h4>
+      <form>
+        <input type="text" id="username" name="username" placeholder="username"></input>
+        <br/>
+        <br/>
+        <input type="password" id="password" name="password"></input>
+        <br/>
+        <br/>
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </form>
     </div>
   );
 }
